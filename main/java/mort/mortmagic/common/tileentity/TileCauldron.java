3688268506e2f3a57fd5be4316d07abfc87efe57
martin.labut <martin.labut@volny.cz>
2017-08-23 20:22:56 +0200

    color interpolation

--- main/java/mort/mortmagic/common/tileentity/TileCauldron.java
+++ main/java/mort/mortmagic/common/tileentity/TileCauldron.java
@@ -7,6 +7,7 @@ import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.Vec3d;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,13 +31,62 @@ public class TileCauldron extends TileEntity {
         return true;
     }
 
+    public static final Vec3d rubToRgb[] = {
+            new Vec3d( 1,1,1 ), new Vec3d(0.163,0.373,0.6), new Vec3d(1,1,0), new Vec3d(0,0.6,0.2),
+            new Vec3d( 1,0,0 ), new Vec3d(0.5,0,0.5), new Vec3d(1,0.5,0), new Vec3d(0.2,0.094,0)
+    };
 
     public int getColor(){
 
-        if( items.size() > 0 )
-            return 564524;
+        if( items.size() == 0 )
+            return 0;
 
-        return 34355 ;
+        float rubedo = 0; int rubedoCnt = 0;
+        float auredo = 0; int auredoCnt = 0;
+        float caerudo = 0; int caerudoCnt = 0;
+
+        for(PotionIngredientRegistry.Entry entry : ingredientCache){
+            if( entry.rubedo > 0 ){
+                rubedo += entry.rubedo;
+                rubedoCnt++;
+            }
+            if( entry.auredo > 0 ){
+                auredo += entry.auredo;
+                auredoCnt++;
+            }
+            if( entry.caerudo > 0 ){
+                caerudo += entry.caerudo;
+                caerudoCnt++;
+            }
+        }
+        rubedo /= rubedoCnt;
+        auredo /= auredoCnt;
+        caerudo /= caerudoCnt;
+
+        Vec3d result = trilinearInterpolation( rubToRgb, auredo, caerudo, rubedo );
+        return (((int)result.x*255) >> 16 ) + (((int)result.y*255) >> 8 ) + ((int)result.z*255);
+    }
+
+
+    private static Vec3d linearInterpolation( Vec3d a, Vec3d b, float f ){
+        return new Vec3d( a.x * f + b.x * (1-f), a.y * f + b.y * (1-f), a.z * f + b.z * (1-f) );
+    }
+
+    /**
+     * i interpolates in numbers, f in characters
+     * @param a1
+     * @param b1
+     * @param a2
+     * @param b2
+     * @param f
+     * @param i
+     * @return
+     */
+    private static Vec3d bilinearInterpolation( Vec3d a1, Vec3d b1, Vec3d a2, Vec3d b2, float f, float i ){
+        return linearInterpolation( linearInterpolation(a1,a2,i),linearInterpolation(b1,b2,i),f );
+    }
+    private static Vec3d trilinearInterpolation( Vec3d[] cube, float f, float i, float z ){
+        return linearInterpolation( bilinearInterpolation(cube[0],cube[1],cube[2],cube[3],f,i), bilinearInterpolation(cube[4],cube[5],cube[6],cube[7],f,i), z );
     }
 
     @Override
